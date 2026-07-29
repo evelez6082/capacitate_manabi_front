a9ad2a110698f0129ac44e5e9b6b4c5c9bec4b12
Jhon Cedeno <jhonalexandrocm@gmail.com>
2026-07-29 12:50:01 -0500

    Fix local Cloudflare compatibility date

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig(async () => {
         config: {
           name: "server",
           main: "./worker/index.ts",
-          compatibility_date: "2026-07-01",
+          compatibility_date: "2026-05-22",
           assets: {
             binding: "ASSETS",
             not_found_handling: "single-page-application",
